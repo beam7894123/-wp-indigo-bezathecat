cef01115ac1a9c54e5a25057cbdcf9f55dd07d8c
beam7894123 <beamtamamoon@gmail.com>
2021-11-05 14:34:25 +0700

    Clone from vitathemes/wp-indigo (2.1.1)

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+sass.compiler = require("node-sass");
+const imagemin = require("gulp-imagemin");
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const browserSync = require("browser-sync").create();
+const series = gulp.series;
+const parallel = gulp.parallel;
+
+const sassTask = (cb) => {
+  return gulp
+    .src("assets/src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("assets/src/css"))
+    .pipe(browserSync.stream());
+  cb();
+};
+
+const cssConcatTask = (cb) => {
+  return gulp
+    .src("assets/src/css/*.css")
+    .pipe(concat("style.css"))
+    .pipe(gulp.dest("assets/css"))
+    .pipe(browserSync.stream());
+  cb();
+};
+
+const cleanCssTask = (cb) => {
+  return gulp
+    .src("assets/css/*.css")
+    .pipe(cleanCSS({ compatibility: "ie11" }))
+    .pipe(gulp.dest("assets/css"));
+  cb();
+};
+
+const concatVendorJs = (cb) => {
+  return gulp
+    .src(["./assets/src/js/masonry.pkgd.js", "./assets/src/js/iconify.js"])
+    .pipe(concat("vendor.js"))
+    .pipe(gulp.dest("assets/js"));
+  cb();
+};
+
+const concatJs = (cb) => {
+  return gulp
+    .src("./assets/src/js/script.js")
+    .pipe(concat("script.js"))
+    .pipe(gulp.dest("./assets/js"));
+  cb();
+};
+
+exports.default = () =>
+  gulp
+    .src("assets/src/images/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("assets/images/dist"));
+
+const browserSyncTask = (cb) => {
+  browserSync.init({
+    proxy: "indigo.local/",
+    ui: false,
+  });
+  cb();
+};
+
+const watchTask = () => {
+  gulp.watch("./assets/src/scss/**/*.scss", series(sassTask, cssConcatTask));
+  gulp.watch(
+    "./assets/src/js/*.js",
+    series(concatJs, concatVendorJs),
+    browserSync.reload
+  );
+  gulp.watch("./**/*.php", browserSync.reload);
+};
+
+exports.default = parallel(
+  series(sassTask, cssConcatTask),
+  series(concatJs, concatVendorJs),
+  series(browserSyncTask, watchTask)
+);
+
+exports.production = parallel(cleanCssTask);
---
+++ template-parts/content.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package wp-indigo
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('c-post'); ?>>
+
+    <section class="c-post__entry-header">
+
+        <div class="c-post__entry-meta <?php wp_indigo_get_entry_meta_class(); ?>">
+
+            <?php
+                if ( is_singular() ) :
+                    the_title( '<h4 class="c-post__entry-title">', '</h4>' );
+                else :              
+                    the_title( '<h4 class="c-post__entry-title"><a  href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h4>' );
+                endif;			            
+            ?>
+
+
+            <div class="c-post__date">
+                <span>
+                    <a class="h6" href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
+                        <?php echo esc_html( get_the_date() ) ?>
+                    </a>
+                </span>
+            </div><!-- c-post__date -->
+
+            <?php if( get_theme_mod( 'archives_posts_category' , true ) ) : ?>
+            <div class="c-post__category">
+                <?php  wp_indigo_get_custom_category('' , 'c-post__cat c-post__cat--blog u-link--secondary h6'); ?>
+            </div>
+            <?php endif; ?>
+
+
+        </div><!-- c-post__entry-meta -->
+
+
+    </section><!-- c-post__entry-header -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
